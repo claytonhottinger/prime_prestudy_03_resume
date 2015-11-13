23e3e63bfd9317ff7625938938e7736ec9dac130
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2015-11-13 11:57:51 -0600

    Change animations to activate using jQuery instead of CSS pseudo classes

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-$(document).ready(function(){
-	$('.button').on('click', function(){
-		$(this).next().removeClass('hidden');
-		$(this).addClass('hidden');
-	});
-	
+$(document).ready(function() {
+  $('.button').on('click', function() {
+    $(this).next().removeClass('hidden');
+    $(this).addClass('hidden');
+  });
+
+  $('.WorkExperience').on('click', function() {
+    if ($(this).hasClass('partyWorkExperience') == false) {
+      alert('PARTYYYYY!!!!!');
+    }
+
+    $(this).toggleClass('partyWorkExperience');
+
+  });
+
+  $('.Education').on('click', function() {
+    if ($(this).hasClass('partyEducation') == false) {
+      alert('PARTYYYYY!!!!!');
+    }
+
+    $(this).toggleClass('partyEducation');
+
+  });
+
+  $('header').on('click', function() {
+    if ($(this).hasClass('partyheader') == false) {
+      alert('PARTYYYYY!!!!!');
+    }
+
+    $(this).toggleClass('partyheader');
+
+  });
+
+  $('.main').on('click', function() {
+    if ($(this).hasClass('partymain') == false) {
+      alert('PARTYYYYY!!!!!');
+    }
+
+    $(this).toggleClass('partymain');
+  });
+
 });
